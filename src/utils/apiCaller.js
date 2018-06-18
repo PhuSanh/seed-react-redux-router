1153cfe4c1c682ee55fc59941711e4da58406c6f
Doan Phu Sanh <dpsanh2348@gmail.com>
2018-06-18 08:54:42 +0700

    update axios api

---
+++ src/utils/apiCaller.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import * as Config from './../constants/Config';
+
+export default callApi = (endpoint, method = 'GET', body = null) => {
+    return axios({
+        method: method,
+        url: `${Config.API_URL}/${endpoint}`,
+        data: body
+    }).catch(err => {
+        console.log(err);
+    })
+}
